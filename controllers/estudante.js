416fdf298beb29a127b88c6df01971f6ee61448d
Ademilson-Chagas-Cappellari <adechagas@gmail.com>
2021-10-14 19:59:07 -0300

    Iniciando a busca de estudantes por matricula

--- controllers/estudante.js
+++ controllers/estudante.js
@@ -2,11 +2,27 @@
 import estudante from "../models/estudante.js"
 
 // Buscar todos os estudantes
-export const getEstudantes = async (req, res) => {
-  try {
-    const estudantes = await estudante.findAll();
-    res.send(estudantes);
-  } catch (erro) {
-    console.log(erro);
-  }
+export const getEstudante = async(req, res) => {
+    try {
+        const estudantes = await estudante.findAll();
+        res.send(estudantes);
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+
+//Buscar estudantes por matricula
+
+export const getEstudanteByMatr = async(req, res) => {
+    try {
+        const estudante = await estudante.findAll({
+            where: {
+                matricula_estudante: req.params.matricula
+            }
+        })
+        res.send(estudantes)
+    } catch (erro) {
+        console.log(erro)
+    }
+
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,14 @@
-
 import express from "express";
 // Importar estudante Controller
 import {
-  getEstudantes,
+    getEstudantes,
 } from "../controllers/estudante.js"
 
 // Inicializar o router express
 const router = express.Router();
 
-router.get('/estudante', getEstudantes)
+router.get('/estudante', getEstudante)
+router.get('/estudante/:matricula', getEstudanteByMatr)
 
 // export router
 export default router
